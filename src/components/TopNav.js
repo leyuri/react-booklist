6f06063563c5e7e7278805cf359ce2632c4d2765
leyuri <leyuri97@gmail.com>
2021-08-04 17:46:15 +0900

    Add : AppBar 를 이용하여 TobNav 구성요소 추가

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-export default function() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+
+  export default function TopNavBar() {
+    const classes = useStyles();
+  
     return (
-        <div>TopNav</div>
-    )
-}
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              BookList
+            </Typography>
+            <Button color="inherit">Add</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
