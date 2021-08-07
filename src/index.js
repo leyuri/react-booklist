1b8000ce7a3e8e63273715e7d5e0fe2d7bb176fb
leyuri <leyuri97@gmail.com>
2021-08-07 23:16:49 +0900

    Add : redux devtools extension 사용하기 위한 설정 추가

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import reducer from './reducer';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware()))
 
 ReactDOM.render(
   <Provider store={store}>
      <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>s
   </Provider>,
   document.getElementById('root')
 );
